813d0e1b7a843d7c243938d7c548a6f078eeeea2
Wojdan88 <wojciech.danczuk@gmail.com>
2017-05-17 23:42:36 +0200

    rezerwowanie, rejestracja, logowanie

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿select distinct Samochody.ID_Samochodu, Kolor, Lokalizacja.Nazwa from Samochody  
+             left join Lokalizacja on Samochody.ID_Samochodu = Lokalizacja.ID_Lokalizacji left join Rezerwacja on   
+             Samochody.ID_Samochodu = Rezerwacja.ID_Samochodu where not exists(Select * From Rezerwacja where   
+             Rezerwacja.ID_Samochodu = Samochody.ID_Samochodu and Data_zwrotu >= '2017-05-26' and Data_Odbioru <= '2017-05-27')
+
+			 Select * From Rezerwacja where Data_zwrotu >= '2017-05-26' and Data_Odbioru <= '2017-
